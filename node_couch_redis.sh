5e3fa0f53e737b6b888f5411795946d2372c4cf5
Geert Pasteels <geert.pasteels@gmail.com>
2012-04-02 07:37:39 -0700

    added pipe sh

--- node_couch_redis.sh
+++ node_couch_redis.sh
@@ -4,16 +4,16 @@
 apt-get install -y curl
 
 # Root ssh keys
-curl https://raw.github.com/Enome/linode-deployment/master/ssh.sh
+curl https://raw.github.com/Enome/linode-deployment/master/ssh.sh | sh
 
 # Update apt-get
-curl https://raw.github.com/Enome/linode-deployment/master/apt-get.sh
+curl https://raw.github.com/Enome/linode-deployment/master/apt-get.sh | sh
 
 # Dependencies
-curl https://raw.github.com/Enome/linode-deployment/master/dependencies.sh
+curl https://raw.github.com/Enome/linode-deployment/master/dependencies.sh | sh
 
 # Git
-curl https://raw.github.com/Enome/linode-deployment/master/git.sh
+curl https://raw.github.com/Enome/linode-deployment/master/git.sh | sh
 
 # Node.js
-curl https://raw.github.com/Enome/linode-deployment/master/node.sh
+curl https://raw.github.com/Enome/linode-deployment/master/node.sh | sh
