809cc8317d26fee5447410c2a01cb73103a0ce55
Geert Pasteels <geert.pasteels@gmail.com>
2012-04-07 13:23:34 -0700

    hooks not books!

--- deployment.sh
+++ deployment.sh
@@ -2,4 +2,4 @@ mkdir /root/repository
 mkdir /root/repository/app
 git init /root/repository/app --bare
 curl https://raw.github.com/Enome/linode-deployment/master/express-post-receive > /root/repository/app/hooks/post-receive
-chmod +x /root/repository/app/books/post-receive
+chmod +x /root/repository/app/hooks/post-receive
