8f1dd46168b310a7312f4ac6213d1e1af1e924e6
Geert Pasteels <geert.pasteels@gmail.com>
2012-04-08 02:40:44 -0700

    updated deployment

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,12 @@
+app="first"
+
+# Bare respo
+
 mkdir /root/repository
-mkdir /root/repository/app
-git init /root/repository/app --bare
+mkdir /root/repository/${app}
+git init /root/repository/${app} --bare
+
+# Add post-receive
+
 curl https://raw.github.com/Enome/linode-deployment/master/express-post-receive > /root/repository/app/hooks/post-receive
 chmod +x /root/repository/app/hooks/post-receive
